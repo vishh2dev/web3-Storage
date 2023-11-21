04d0b984c5c58d765c2ad41dfd276443cd4fcdc0
vishh2dev <vishh2dev@gmail.com>
2023-11-21 20:34:45 +0530

    feat: adding file uploader function

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -21,7 +21,7 @@ function App() {
         await provider.send("eth_requestAccounts",[])//to open metamask automatically 
         const signer =await provider.getSigner() 
         const address = await signer.getAddress() // address of connected account
-        console.log('address',address)
+        
         setAccount(address)
         const contractAddresss = '0x5FbDB2315678afecb367f032d93F642f64180aa3' // from upload.sol
         const contract = new ethers.Contract(
@@ -29,7 +29,7 @@ function App() {
           storage.abi,//from the artifacts  
           signer
         )
-        console.log(contract)
+       
         setContract(contract)
         setProvider(signer)
       }else{
@@ -39,9 +39,15 @@ function App() {
    provider && wallet()
   },[])
   return (
-   <div>
-    <h1>Hello</h1>
-    <FileUpload contract={contract} account={account}/>
+   <div className="App">
+      <h1 style={{ color: "white" }}>Gdrive 3.0</h1>
+      <div className="bg"></div>
+      <div className="bg bg2"></div>
+      <div className="bg bg3"></div>
+      <p style={{ color: "white" }}>
+        Account : {account ? account : "Not connected"}
+      </p>
+      <FileUpload contract={contract} account={account} />
    </div>
   )
 }
--- client/src/components/FileUpload.jsx
+++ client/src/components/FileUpload.jsx
@@ -1,8 +1,52 @@
+import { useState } from "react";
+import "./FileUpload.css";
+export const FileUpload = ({contract,account}) => {
+  const[file,setFile] = useState(null)
+  const[fileName,setFileName] = useState(null)
 
-export const FileUpload = () => {
+  const retrieveFile = (e) =>{
+    const data = e.target.files[0];
+    const reader = new window.FileReader()
+    reader.readAsArrayBuffer(data)
+    reader.onloadend = () =>{
+      setFile(e.target.files[0])
+    }
+    setFileName(e.target.files[0].name)
+    e.preventDefault()
+  }
+
+ 
+
+  const handleSubmit = (e) =>{
+    e.preventDefault();
+    if(file){
+      try {
+        const formData = new FormData();
+        formData.append("file", file);
+      } catch (e) {
+        alert("Unable to upload image to Pinata");
+      }
+    }
+  }
   return (
+
     <div>
-      hey
+        <form className="form" onSubmit={handleSubmit}>
+        <label htmlFor="file-upload" className="choose">
+          Choose Image
+        </label>
+        <input
+          disabled={!account}
+          type="file"
+          id="file-upload"
+          name="data"
+          onChange={retrieveFile}
+        />
+        <span className="textArea">Image: </span>
+        <button type="submit" className="upload" >
+          Upload File
+        </button>
+      </form>
     </div>
   )
 }
